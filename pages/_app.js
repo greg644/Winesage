2c7a71acedeb759d2d0d99bddae83e7b3e153594
greg644 <greg@mceneny.net>
2026-05-09 10:13:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,9 @@ import { useEffect } from 'react'
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js')
+      navigator.serviceWorker.getRegistrations().then(regs => {
+        regs.forEach(reg => reg.unregister());
+      });
     }
   }, [])
   return <Component {...pageProps} />
